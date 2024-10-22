bb6cdc91636b309e92ec6674671c1e56d25bb066
sudeeya <pyrkin.aa@phystech.edu>
2024-10-22 13:29:11 +0300

    Add raw server config

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -1,8 +1,19 @@
 package config
 
+import "github.com/caarlos0/env"
+
 type Config struct {
+	Address     string `env:"ADDRESS" envDefault:"localhost:8080"`
+	DatabaseDSN string `env:"DATABASE_DSN,required"`
+	LogLevel    string `env:"LOG_LEVEL" envDefault:"INFO"`
+	LogFile     string `env:"LOG_FILE,required"`
 }
 
 func NewConfig() (*Config, error) {
-	return nil, nil
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
