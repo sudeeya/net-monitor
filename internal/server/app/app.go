4505343bd1f147852b338edd762c3694e5cf6dd6
sudeeya <pyrkin.aa@phystech.edu>
2024-10-27 18:39:48 +0300

    Add Run and Shutdown methods for server

--- internal/server/app/app.go
+++ internal/server/app/app.go
@@ -1,7 +1,11 @@
 package app
 
 import (
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"go.uber.org/zap"
 
@@ -34,6 +38,35 @@ func NewApp(
 	}
 }
 
-func (s *app) Run() {
-	panic("unimplemented")
+func (a *app) Run() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	go func() {
+		if err := http.ListenAndServe(a.cfg.HTTPAddr, a.handler); err != nil {
+			a.logger.Fatal(err.Error())
+		}
+	}()
+
+	go func() {
+		listen, err := net.Listen("tcp", a.cfg.GRPCAddr)
+		if err != nil {
+			a.logger.Fatal(err.Error())
+		}
+
+		if err := a.grpcServer.Serve(listen); err != nil {
+			a.logger.Fatal(err.Error())
+		}
+	}()
+
+	<-sigCh
+	a.Shutdown()
+}
+
+func (a *app) Shutdown() {
+	if err := a.logger.Sync(); err != nil {
+		a.logger.Sugar().Fatalf("failed to sync logger: %v", err)
+	}
+
+	os.Exit(0)
 }
