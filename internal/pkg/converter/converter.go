dd2be657c61ec72be76e2a98573fb6bba50c1823
sudeeya <pyrkin.aa@phystech.edu>
2025-02-16 19:08:59 +0300

    Edit converter

--- internal/pkg/converter/converter.go
+++ internal/pkg/converter/converter.go
@@ -3,7 +3,6 @@ package converter
 
 import (
 	"errors"
-	"net"
 	"net/netip"
 
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -17,7 +16,6 @@ var errorPrefixNoSlash = errors.New("no '/'")
 // ToProtoFromSnapshot converts model representation of snapshot to protobuf.
 func ToProtoFromSnapshot(snapshot *model.Snapshot) *pb.Snapshot {
 	devices := make([]*pb.Snapshot_Device, len(snapshot.Devices))
-
 	for deviceIdx, device := range snapshot.Devices {
 		devices[deviceIdx] = ToProtoFromDevice(device)
 	}
@@ -31,37 +29,34 @@ func ToProtoFromSnapshot(snapshot *model.Snapshot) *pb.Snapshot {
 // ToProtoFromDevice converts model representation of device to protobuf.
 func ToProtoFromDevice(device model.Device) *pb.Snapshot_Device {
 	ifaces := make([]*pb.Snapshot_Device_Interface, len(device.Interfaces))
-
 	for ifaceIdx, iface := range device.Interfaces {
 		ifaces[ifaceIdx] = ToProtoFromInterface(iface)
 	}
 
 	return &pb.Snapshot_Device{
-		Hostname:     device.Hostname,
-		Vendor:       device.Vendor,
-		OsName:       device.OSName,
-		OsVersion:    device.OSVersion,
-		Serial:       device.Serial,
-		ManagementIp: netip.Prefix(device.ManagementIP).String(),
-		Interfaces:   ifaces,
+		Hostname:             device.Hostname,
+		Vendor:               device.Vendor,
+		OsName:               device.OSName,
+		OsVersion:            device.OSVersion,
+		Serial:               device.Serial,
+		IsSnapshotSuccessful: device.IsSnapshotSuccessful,
+		Interfaces:           ifaces,
 	}
 }
 
 // ToProtoFromInterface converts model representation of interface to protobuf.
 func ToProtoFromInterface(iface model.Interface) *pb.Snapshot_Device_Interface {
 	return &pb.Snapshot_Device_Interface{
-		Name:      iface.Name,
-		Mac:       net.HardwareAddr(iface.MAC).String(),
-		Ip:        netip.Prefix(iface.IP).String(),
-		Mtu:       iface.MTU,
-		Bandwidth: iface.Bandwidth,
+		Name: iface.Name,
+		IsUp: iface.IsUp,
+		Ip:   iface.IP.String(),
+		Mtu:  iface.MTU,
 	}
 }
 
 // ToDeviceFromProto converts protobuf representation of snapshot to model.
 func ToSnapshotFromProto(snapshot *pb.Snapshot) (*model.Snapshot, error) {
 	devices := make([]model.Device, len(snapshot.Devices))
-
 	for deviceIdx, device := range snapshot.Devices {
 		d, err := ToDeviceFromProto(device)
 		if err != nil {
@@ -80,14 +75,6 @@ func ToSnapshotFromProto(snapshot *pb.Snapshot) (*model.Snapshot, error) {
 // ToDeviceFromProto converts protobuf representation of device to model.
 func ToDeviceFromProto(device *pb.Snapshot_Device) (*model.Device, error) {
 	ifaces := make([]model.Interface, len(device.Interfaces))
-
-	managementIP, err := netip.ParsePrefix(device.ManagementIp)
-	if err != nil {
-		if errors.Is(err, errorPrefixNoSlash) {
-			return nil, err
-		}
-	}
-
 	for ifaceIdx, iface := range device.Interfaces {
 		i, err := ToInterfaceFromProto(iface)
 		if err != nil {
@@ -98,27 +85,18 @@ func ToDeviceFromProto(device *pb.Snapshot_Device) (*model.Device, error) {
 	}
 
 	return &model.Device{
-		Hostname:     device.Hostname,
-		Vendor:       device.Vendor,
-		OSName:       device.OsName,
-		OSVersion:    device.OsVersion,
-		Serial:       device.Serial,
-		ManagementIP: managementIP,
-		Interfaces:   ifaces,
+		Hostname:             device.Hostname,
+		Vendor:               device.Vendor,
+		OSName:               device.OsName,
+		OSVersion:            device.OsVersion,
+		Serial:               device.Serial,
+		IsSnapshotSuccessful: device.IsSnapshotSuccessful,
+		Interfaces:           ifaces,
 	}, nil
 }
 
 // ToInterfaceFromProto converts protobuf representation of interface to model.
 func ToInterfaceFromProto(iface *pb.Snapshot_Device_Interface) (*model.Interface, error) {
-	var mac net.HardwareAddr
-	if iface.Mac != "" {
-		m, err := net.ParseMAC(iface.Mac)
-		if err != nil {
-			return nil, err
-		}
-		mac = m
-	}
-
 	ip, err := netip.ParsePrefix(iface.Ip)
 	if err != nil {
 		if errors.Is(err, errorPrefixNoSlash) {
@@ -127,10 +105,9 @@ func ToInterfaceFromProto(iface *pb.Snapshot_Device_Interface) (*model.Interface
 	}
 
 	return &model.Interface{
-		Name:      iface.Name,
-		MAC:       mac,
-		IP:        ip,
-		MTU:       iface.Mtu,
-		Bandwidth: iface.Bandwidth,
+		Name: iface.Name,
+		IsUp: iface.IsUp,
+		IP:   ip,
+		MTU:  iface.Mtu,
 	}, nil
 }
